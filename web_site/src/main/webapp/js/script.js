684057779ae9397250b00bec8a5eef13c06e4fa5
jungmook Knag <106642094+kangjungmook@users.noreply.github.com>
2023-07-11 11:47:13 +0900

    Update script.js

--- web_site/src/main/webapp/js/script.js
+++ web_site/src/main/webapp/js/script.js
@@ -1,1 +1,4 @@
 document.querySelector('.menu-btn').addEventListener('click', () => document.querySelector('.main-menu').classList.toggle('show'));
+
+const todayDiv = document.getElementById("today");
+const timeDiv = document.getElementById("time");
